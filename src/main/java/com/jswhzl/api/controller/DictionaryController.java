869d03eb1a4679f0005537065079126f2f81c0e7
kiocahduaf <1009470059@qq.com>
2019-01-30 10:46:40 +0800

    修改配置

--- src/main/java/com/jswhzl/api/controller/DictionaryController.java
+++ src/main/java/com/jswhzl/api/controller/DictionaryController.java
@@ -1,34 +1,38 @@
 package com.jswhzl.api.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
-import com.jswhzl.api.service.TokenService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jswhzl.api.entity.Dictionary;
+import com.jswhzl.api.service.TokenService;
+import com.jswhzl.api.service.impl.DictionaryServiceImpl;
 import com.jswhzl.common.annotation.SysLog;
 import com.jswhzl.common.base.BaseController;
-import org.springframework.validation.FieldError;
 import com.jswhzl.common.bean.ReturnEntity;
 import com.jswhzl.common.bean.ReturnEntityError;
 import com.jswhzl.common.bean.ReturnEntitySuccess;
 import com.jswhzl.common.config.Constants;
 import com.jswhzl.common.util.Pager;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import com.jswhzl.api.service.impl.DictionaryServiceImpl;
-import com.jswhzl.api.entity.Dictionary;
-import java.time.LocalDateTime;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 /**
  * <p>
  *  前端控制器
